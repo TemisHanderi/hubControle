5977514a94cc6b0c224b7cd7fdb19e2bba4b547d
TemisL <temishanderil@gmail.com>
2025-07-07 20:26:14 -0300

    Criação tela login

---
+++ login/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const botaoEntrar = document.getElementById('botaoEntrar');
+
+  botaoEntrar.addEventListener('click', () => {
+    const nome = document.getElementById("nomeUsuario").value.trim();
+
+    if (nome === "") {
+      alert("Por favor, digite seu nome.");
+      return;
+    }
+
+    localStorage.setItem("nomeUsuario", nome);
+
+    document.body.classList.add('fade-out');
+    setTimeout(() => {
+      window.location.href = "../home/index.html";
+    }, 500); // esperar 0.5s
+  });
+});
